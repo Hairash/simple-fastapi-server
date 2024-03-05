86c62cdec22b978e340d3bbba3b1ac4f1d8a14b2
Ivan Grebenshchikov <Ivan.Grebenshchikov@ingrammicro.com>
2024-03-05 17:45:02 +0100

    Decode messages
    * Add sign-up page

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import base64
+import json
+
 from fastapi import FastAPI, Request
 
 app = FastAPI()
@@ -9,6 +12,10 @@ app = FastAPI()
 
 @app.post("/webhook/")
 async def receive_webhook(request: Request):
-    body = await request.json()
-    print(f"Received webhook: {body}")
-    return {"message": "Received successfully", "body": body}
+    request_json = await request.json()
+    encoded_data = request_json['message']['data']
+    data = json.loads(base64.b64decode(encoded_data).decode('utf-8'))
+    request_json['message']['data'] = data
+    print(f'Received webhook: {json.dumps(request_json, indent=4)}')
+    # TODO: Hide decoded data from response
+    return {"message": "Received successfully", "body": request_json}
