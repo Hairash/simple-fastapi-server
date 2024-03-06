d01b059943744a4f1c50c8abc26e3506781f5b1f
Ivan Grebenshchikov <Ivan.Grebenshchikov@ingrammicro.com>
2024-03-06 16:27:19 +0100

    Try to fix

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ async def receive_webhook(request: Request):
 
 
 @app.post("/signup")
-async def handle_signup(token: str = Form(...)):
+async def handle_signup(request: Request):
     # Attempt to extract the JWT from the x-gcp-marketplace-token header
     # jwt_token = request.headers.get("x-gcp-marketplace-token", "No JWT token provided")
-    print(f'Received x-gcp-marketplace-token: {token}')
+    # print(f'Received x-gcp-marketplace-token: {token}')
 
     # For demonstration, print the JWT token to console
     # print(f"Received JWT token: {jwt_token}")
@@ -38,11 +38,18 @@ async def handle_signup(token: str = Form(...)):
     # except Exception as e:
     #     print(e)
     #
-    # try:
-    #     request_body = await request.body()
-    #     print(f'Request body: {request_body}')
-    # except Exception as e:
-    #     print(e)
+    try:
+        request_body = await request.body()
+        print(f'Request body: {request_body}')
+    except Exception as e:
+        print(e)
+
+    try:
+        form_data = await request.form()
+        token = form_data.get('x-gcp-marketplace-token')
+        print(f'Received x-gcp-marketplace-token: {token}')
+    except Exception as e:
+        print(e)
 
     # If you need to parse and use the JWT token, do so here
     # try:
